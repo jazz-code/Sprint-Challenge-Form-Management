c8b78129a38f7df450cd52deb4a5b5e8122fc57f
jazz-code <ren.meta@gmail.com>
2019-08-09 12:21:00 -0400

    Yup validation works

--- client/src/components/UserForm.js
+++ client/src/components/UserForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Formik, Form, Field, withFormik } from "formik";
+import { Formik, Form, Field, withFormik, validateYupSchema } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
@@ -10,6 +10,7 @@ const UserForm = () => {
       <Form>
         <Field type="text" name="username" placeholder="Username" />
         <Field type="text" name="password" placeholder="Password" />
+        <button type="submit">Submit</button>
       </Form>
     </div>
   );
@@ -21,6 +22,18 @@ const FormikForm = withFormik({
       username: username || "",
       password: password || ""
     };
+  },
+
+  validationSchema: Yup.object().shape({
+    username: Yup.string().required("Username is Required"),
+    password: Yup.string().required("Password is Required")
+  }),
+
+  handleSubmit(values, { resetForm }) {
+    axios
+      .post(`http://localhost:5000/api/register`, values)
+      .then(res => console.log(res));
+    resetForm();
   }
 })(UserForm);
 
