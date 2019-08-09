76388fa5575580c05d8b79ddf174324d2e989ffb
jazz-code <ren.meta@gmail.com>
2019-08-09 12:05:16 -0400

    more dependencies, User Form username and password layout with Formik

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
+import UserForm from "./components/UserForm";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
+      <UserForm />
     </div>
   );
 }
---
+++ client/src/components/UserForm.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import { Formik, Form, Field, withFormik } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+
+const UserForm = () => {
+  return (
+    <div className="form">
+      <h1>Form</h1>
+      <Form>
+        <Field type="text" name="username" placeholder="Username" />
+        <Field type="text" name="password" placeholder="Password" />
+      </Form>
+    </div>
+  );
+};
+
+export default UserForm;
